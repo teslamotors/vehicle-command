teslamotors/vehicle-command

commit beb8735ec5fa86de1ca60cfee52f1fe784521cfc

Fix race condition in ErrProtocolNotSupported
Fixed a race condition that caused the library (and http proxy) to not
recognize ErrProtocolNotSupported, which indicates the vehicle does not
support the newer protocol.

The race condition was caused by code that attempted to handshake with
multiple vehicle subsystems (domains) in parallel. If one subsystem
returned ErrProtocolNotSupported, the pending requests to other
subsystems were canceled. In some cases, the context cancellation error
would be propagated to the down the stack instead of
ErrProtocolNotSupported.

With this fix, the library prioritizes non-context cancellation errors.

internal/dispatcher/dispatcher.go
@@ -3,6 +3,7 @@ package dispatcher
 import (
 	"context"
 	"crypto/rand"
+	"errors"
 	"fmt"
 	"sync"
 	"time"
@@ -110,13 +111,16 @@ func (d *Dispatcher) StartSessions(ctx context.Context, domains []universal.Doma
 			results <- d.StartSession(aggregateContext, dom)
 		}(domain)
 	}
+	var err error
 	for i := 0; i < len(domains); i++ {
-		err := <-results
-		if err != nil {
+		err = <-results
+		// The aggregateContext is canceled if one of the handshakes fails. We don't want to return
+		// the Canceled error if ErrProtocolNotSupported is present.
+		if !errors.Is(err, context.Canceled) {
 			return err
 		}
 	}
-	return nil
+	return err
 }
 
 func (d *Dispatcher) createHandler(key *receiverKey) *receiver {

pkg/proxy/proxy.go
@@ -263,7 +263,7 @@ func (p *Proxy) handleVehicleCommand(acct *account.Account, w http.ResponseWrite
 	}
 	defer car.Disconnect()
 
-	if err := car.StartSession(ctx, nil); err == protocol.ErrProtocolNotSupported {
+	if err := car.StartSession(ctx, nil); errors.Is(err, protocol.ErrProtocolNotSupported) {
 		p.markUnsupportedVIN(vin)
 		p.forwardRequest(acct.Host, w, req)
 		return err

commit a55d86206ec43b6525b20c441f44eafbd3dbfab9

Fix regression in session info
Commit beb8735ec5fa86de1ca60cfee52f1fe784521cfc introduced a
regression that prevents clients from establishing session info on
connections that are not cached. It caused a handshake to one domain to
be cancelled when a handshake to another domain succeeded.

internal/dispatcher/dispatcher.go
@@ -116,7 +116,7 @@ func (d *Dispatcher) StartSessions(ctx context.Context, domains []universal.Doma
 		err = <-results
 		// The aggregateContext is canceled if one of the handshakes fails. We don't want to return
 		// the Canceled error if ErrProtocolNotSupported is present.
-		if !errors.Is(err, context.Canceled) {
+		if err != nil && !errors.Is(err, context.Canceled) {
 			return err
 		}
 	}

internal/dispatcher/dispatcher_test.go
@@ -19,6 +19,7 @@ import (
 )
 
 var errOutboxFull = errors.New("dispatcher: outbox full")
+var errDropMessage = errors.New("dispatcher: simulated dropped message")
 var errTimeout = errors.New("dispatcher: simulated timeout")
 var testPayload = []byte("ack")
 var quiescentDelay = 250 * time.Millisecond
@@ -246,7 +247,11 @@ func (d *dummyConnector) Send(ctx context.Context, buffer []byte) error {
 		err := d.errorQueue[0]
 		d.errorQueue = d.errorQueue[1:]
 		d.lock.Unlock()
-		return err
+		if err == errDropMessage {
+			return nil
+		} else if err != nil {
+			return err
+		}
 	}
 	if err := proto.Unmarshal(buffer, &message); err != nil {
 		return err
@@ -643,6 +648,36 @@ func TestConnect(t *testing.T) {
 
 	if _, err := dispatcher.Send(ctx, testCommand(), connector.AuthMethodHMAC); err != nil {
 		t.Errorf("Unexpected error: %s", err)
+	}
+}
+
+func TestWaitForAllSessions(t *testing.T) {
+	conn := newDummyConnector(t)
+	defer conn.Close()
+
+	// Configure the Connector to only respond to the first of two handshakes
+	conn.EnqueueSendError(nil)
+	conn.EnqueueSendError(errDropMessage)
+
+	key, err := authentication.NewECDHPrivateKey(rand.Reader)
+	if err != nil {
+		t.Fatalf("Couldn't create private key: %s", err)
+	}
+
+	dispatcher, err := New(conn, key)
+	if err != nil {
+		t.Fatalf("Couldn't initialize dispatcher: %s", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), quiescentDelay)
+	defer cancel()
+
+	if err := dispatcher.Start(ctx); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := dispatcher.StartSessions(ctx, nil); err != context.DeadlineExceeded {
+		t.Fatalf("Unexpected error: %s", err)
 	}
 }
 