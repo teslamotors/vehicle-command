eaf6ee1fee2bcc2bcbb44dfea921e9fab78ef249
Seth Terashima <sterashima@tesla.com>
2024-07-01 18:07:53 -0700

    Improve BLE reliability
     - Increase timeouts for BLE dialer and listener
     - Retry initial connection on non-fatal errors until context expires
     - Tweak scanning interval to reflect BLE advertisement parameters
    
    Ideas for future improvements:
    
     - Retry connection using client MAC address instead of scanning
     - Optimize scanning interval
     - Adjust timeouts using caller context

--- pkg/connector/ble/device_linux.go
+++ pkg/connector/ble/device_linux.go
@@ -3,10 +3,24 @@ package ble
 import (
 	"github.com/go-ble/ble"
 	"github.com/go-ble/ble/linux"
+	"github.com/go-ble/ble/linux/hci/cmd"
+	"time"
 )
 
+const bleTimeout = 20 * time.Second
+
+// TODO: Depending on the model and state, BLE advertisements come every 20ms or every 150ms.
+
+var scanParams = cmd.LESetScanParameters{
+	LEScanType:           1,    // Active scanning
+	LEScanInterval:       0x10, // 10ms
+	LEScanWindow:         0x10, // 10ms
+	OwnAddressType:       0,    // Static
+	ScanningFilterPolicy: 2,    // Basic filtered
+}
+
 func newDevice() (ble.Device, error) {
-	device, err := linux.NewDevice()
+	device, err := linux.NewDevice(ble.OptListenerTimeout(bleTimeout), ble.OptDialerTimeout(bleTimeout), ble.OptScanParams(scanParams))
 	if err != nil {
 		return nil, err
 	}
