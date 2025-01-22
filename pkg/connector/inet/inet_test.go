d2dbec3a18e2957b9f6f3f0f8ce8173069442066
Seth Terashima <sterashima@tesla.com>
2025-01-22 11:52:34 -0800

    Fix panic on inet Send/Close race
    The Send method of the Connector interface should be safe to run from a
    goroutine, as is done by default when first connecting to a vehicle.
    However, if the inet Connector implementation is closed before the
    client receives a response, this results in Send writing to a closed
    channel and causing a panic.

---
+++ pkg/connector/inet/inet_test.go
@@ -0,0 +1,28 @@
+package inet
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/teslamotors/vehicle-command/pkg/protocol"
+)
+
+func TestSendAfterClose(t *testing.T) {
+	server := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte(`{"response": ""}`))
+	}))
+	defer server.Close()
+	domain, _ := strings.CutPrefix(server.URL, "https://")
+	conn := NewConnection("VIN123", "", domain, "")
+	conn.client = server.Client()
+	if err := conn.Send(context.Background(), []byte{}); err != nil {
+		t.Errorf("Send failed: %s", err)
+	}
+	conn.Close()
+	if err := conn.Send(context.Background(), []byte{}); err != protocol.ErrNotConnected {
+		t.Errorf("Expected ErrNotConnected but got %s", err)
+	}
+}
