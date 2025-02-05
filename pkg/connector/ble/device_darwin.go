aa48216fb0840b6dd3c23cbf719d41fa8dd6d2ac
Lenart Kos <koslenart@gmail.com>
2025-02-05 11:29:55 +0100

    Add warning for using bdAddress on darwin

--- pkg/connector/ble/device_darwin.go
+++ pkg/connector/ble/device_darwin.go
@@ -3,9 +3,13 @@ package ble
 import (
 	"github.com/go-ble/ble"
 	"github.com/go-ble/ble/darwin"
+	"github.com/teslamotors/vehicle-command/internal/log"
 )
 
-func newDevice(ble.Addr) (ble.Device, error) {
+func newDevice(bdAddr ble.Addr) (ble.Device, error) {
+	if bdAddr != nil || bdAddr.String() != "" {
+		log.Warning("Setting the Bluetooth device address is not supported on Darwin")
+	}
 	device, err := darwin.NewDevice()
 	if err != nil {
 		return nil, err
