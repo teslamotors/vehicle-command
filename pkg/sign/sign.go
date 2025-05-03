5f00d5b34de9ec2dc1f56f38636bd9980656168a
Ondrej Belusky <zlymeda@gmail.com>
2025-05-04 00:25:35 +0200

    upgrade to go 1.24.2; upgrade linter to 2.1.5, migrate linter config and fix issues found by the linter

--- pkg/sign/sign.go
+++ pkg/sign/sign.go
@@ -5,21 +5,21 @@ import (
 	"github.com/teslamotors/vehicle-command/internal/authentication"
 )
 
-// SignMessageForVehicle returns a JWT with the provided claims. Only the vehicle with the given VIN
+// MessageForVehicle returns a JWT with the provided claims. Only the vehicle with the given VIN
 // will accept the JWT. To create a JWT that is valid for all vehicles in a fleet, use
-// [SignMessageForFleet].
+// [MessageForFleet].
 //
 // The function overwrites the audience ("aud") and issuer ("iss") JWT claims.
-func SignMessageForVehicle(privateKey authentication.ECDHPrivateKey, vin, app string, message jwt.MapClaims) (string, error) {
+func MessageForVehicle(privateKey authentication.ECDHPrivateKey, vin, app string, message jwt.MapClaims) (string, error) {
 	return authentication.SignMessage(privateKey, message, "com.tesla.vehicle."+vin+"."+app)
 }
 
-// SignMessageForFleet returns a JWT with the provided claims. All vehicles that trust privateKey
+// MessageForFleet returns a JWT with the provided claims. All vehicles that trust privateKey
 // will accept the JWT. To create a JWT that is valid for a single vehicle, use
-// [SignMessageForVehicle].
+// [MessageForVehicle].
 //
 // The function overwrites the audience ("aud") and issuer ("iss") JWT claims.
-func SignMessageForFleet(privateKey authentication.ECDHPrivateKey, app string, message jwt.MapClaims) (string, error) {
+func MessageForFleet(privateKey authentication.ECDHPrivateKey, app string, message jwt.MapClaims) (string, error) {
 	// Issuers are identified by their public key
 	return authentication.SignMessage(privateKey, message, "com.tesla.fleet."+app)
 }
